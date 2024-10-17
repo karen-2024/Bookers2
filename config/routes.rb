687468b1f3029fa3eeb26ee03a391f52af186b27
karen-2024 <177392295+karen-2024@users.noreply.github.com>
2024-10-17 13:19:23 +0000

    [Add] 削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   
-  resources :books, only: [:new, :create, :index, :show]
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+  
   
   get '/homes/about' => 'homes#about', as: 'about'
   
