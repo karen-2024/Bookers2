40501f58441828c60aaf4612f43cad6f47f80def
karen-2024 <177392295+karen-2024@users.noreply.github.com>
2024-10-17 11:44:23 +0000

    [Add] books_new作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,14 @@
 class BooksController < ApplicationController
+  
   def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
   end
 
   def index
@@ -7,4 +16,15 @@ class BooksController < ApplicationController
 
   def show
   end
+  
+  def destroy
+  end
+  
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
