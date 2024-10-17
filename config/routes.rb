cf0fd93b4e2c9b2b832ad8ce1d18d25950ec1b7b
karen-2024 <177392295+karen-2024@users.noreply.github.com>
2024-10-17 13:51:28 +0000

    [add] users_controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   
-  
   devise_for :users
   root to: "homes#top"
   
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
-  
+  resources :users, only: [:show, :edit]
   
   get '/homes/about' => 'homes#about', as: 'about'
   
